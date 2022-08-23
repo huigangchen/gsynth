d547b377f230fdb6dc20d07f805aff0aac2f501f
Huigang Chen <huigang@gmail.com>
2022-08-23 11:46:45 -0700

    Fix MC estimation bug when there is no variation in the covariates during bootstrapping

--- R/core.R
+++ R/core.R
@@ -2362,6 +2362,9 @@ synth.boot<-function(Y,
                 } else {
                     X.boot <- X
                 }
+                if (sd(X.boot)==0) {
+                    X.boot <- array(0, dim=c(dim(X.boot)[1], dim(X.boot)[2], 0))
+                }
                 D.boot <- D[,boot.id]
                 I.boot <- I[,boot.id]
                 W.boot <- NULL
